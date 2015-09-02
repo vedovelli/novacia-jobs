6b0ba696c58e872d7fc7a17b03891f6495e15991
Vedovelli <vedovelli@gmail.com>
2015-09-02 17:40:18 -0300

    Jobs Table Seeder. Please run php artisan db:seed

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
+
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
@@ -17,5 +8,13 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->email,
         'password' => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Job::class, function (Faker\Generator $faker) {
+    return [
+        'jobs_nome' => $faker->name,
+        'jobs_cliente' => $faker->name,
+        'jobs_responsavel' => $faker->name,
     ];
 });
