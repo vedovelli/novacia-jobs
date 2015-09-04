1d19f01d8826e97e95cc6e8447bf9a63e7a14e86
Vedovelli <vedovelli@gmail.com>
2015-09-04 14:50:14 -0300

    Adicao do Bootstrap

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -3,23 +3,37 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
 </head>
 <body>
-	<table>
-		<tr>	
-			<td>Nome</td>
-			<td>Cliente</td>
-			<td>Reponsavel</td>
-		</tr>
-		
-		@foreach($jobs as $job)
-		<tr>	
-			<td>{!! $job->jobs_nome !!}</td>
-			<td>{!! $job->jobs_cliente !!} </td>
-			<td>{!! $job->jobs_responsavel !!} </td>
-		</tr>
-		@endforeach
-		
-	</table>	
+
+	<div class="container">
+
+		<h2>Nova Cia <small>Controle de Jobs</small></h2>
+
+		<table class="table table-bordered table-striped">
+			<thead>
+				<tr>
+					<th>Nome</th>
+					<th>Cliente</th>
+					<th>Reponsavel</th>
+				</tr>
+			</thead>
+
+			<tbody>
+			@foreach($jobs as $job)
+				<tr>
+					<td>{!! $job->jobs_nome !!}</td>
+					<td>{!! $job->jobs_cliente !!} </td>
+					<td>{!! $job->jobs_responsavel !!} </td>
+				</tr>
+			@endforeach
+			</tbody>
+
+		</table>
+	</div>
+
 </body>
 </html>
